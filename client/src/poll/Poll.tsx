b747ac441e1d195dba23d1dc02d86fea7e5d507f
sheldon <sheldonlau1@gmail.com>
2025-05-04 18:39:01 -0700

    move logic to hooks

--- client/src/poll/Poll.tsx
+++ client/src/poll/Poll.tsx
@@ -1,134 +1,22 @@
 import { Checkbox } from "@mui/material";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useParams } from "react-router";
 import "./Poll.less";
+import usePollData from "./usePollData";
+import useQuestions from "./useQuestions";
 
-interface Question {
-  id: number;
-  value: string;
-  votes: number;
-}
-interface PollData {
-  id: string;
-  name: string;
-}
 function Poll() {
   let params = useParams();
   const [inputText, setInputText] = useState("");
-  const [selectedQuestions, setSelectedQuestions] = useState(new Set());
-  useEffect(() => {
-    if (params.pollId) {
-      const dataString = localStorage.getItem(params.pollId);
-      if (dataString) {
-        setSelectedQuestions(new Set(JSON.parse(dataString)));
-      }
-    }
-  }, []);
-
-  const queryClient = useQueryClient();
-  const toggleVote = async (question: any) => {
-    const checked = selectedQuestions.has(question.id);
-
-    console.log(`check value: ${checked}, questionId: ${question.id}`);
-    const response = await fetch(`/api/questions/${question.id}/vote`, {
-      method: "PATCH",
-      body: JSON.stringify({ checked }),
-    });
-    if (params.pollId) {
-      localStorage.setItem(
-        params.pollId,
-        JSON.stringify(Array.from(selectedQuestions))
-      );
-    }
-
-    const data = await response.json();
-    return data;
-  };
-
-  const getQuestions = async (): Promise<Question[]> => {
-    const response = await fetch(`/api/polls/${params.pollId}/questions`, {
-      method: "GET",
-    });
-    const data = await response.json();
-    return data.sort((a: any, b: any) => a.id - b.id);
-  };
-
-  const getPollData = async (): Promise<PollData> => {
-    const response = await fetch(`/api/polls/${params.pollId}`, {
-      method: "GET",
-    });
-    const data = await response.json();
-    return data;
-  };
-  const pollQuery = useQuery({ queryKey: ["poll"], queryFn: getPollData });
-  const questionsQuery = useQuery({
-    queryKey: ["questions"],
-    queryFn: getQuestions,
-  });
-  const createQuestion = async () => {
-    const response = await fetch(`/api/polls/${params.pollId}/questions`, {
-      method: "POST",
-      body: JSON.stringify({ value: inputText }),
-    });
-    const data = await response.json();
-    return data;
-  };
-
-  const createQuestionMutation = useMutation({
-    mutationFn: createQuestion,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["questions"] });
-    },
-  });
-  const onVoteMutation = async (currentQuestion: any) => {
-    let checked = true;
-    if (selectedQuestions.has(currentQuestion.id)) {
-      checked = false;
-      selectedQuestions.delete(currentQuestion.id);
-    } else {
-      selectedQuestions.add(currentQuestion.id);
-    }
-    if (params.pollId) {
-      localStorage.setItem(
-        params.pollId,
-        JSON.stringify(Array.from(selectedQuestions))
-      );
-      setSelectedQuestions(new Set(selectedQuestions));
-    }
-    await queryClient.cancelQueries({ queryKey: ["questions"] });
-    const previousQuestions = queryClient.getQueryData(["questions"]);
-    queryClient.setQueryData(["questions"], (oldQuestions: any) => {
-      const newQuestions = oldQuestions
-        .map((question: any) => {
-          let newQuestion = { ...question };
-          if (currentQuestion.id === question.id) {
-            newQuestion.votes = newQuestion.votes + (checked ? 1 : -1);
-          }
-          return newQuestion;
-        })
-        .sort((a: any, b: any) => a.id - b.id);
-      return newQuestions;
-    });
-    return { previousQuestions };
-  };
-
-  const toggleVoteMutation = useMutation({
-    mutationFn: toggleVote,
-    onMutate: onVoteMutation,
-    onError: (_err, _newTodo, context) => {
-      queryClient.setQueryData(["questions"], context?.previousQuestions);
-    },
-    onSettled: () => {
-      queryClient.invalidateQueries({ queryKey: ["questions"] });
-    },
-  });
+  const pollData = usePollData(params.pollId);
+  const { questions, selectedQuestions, toggleVote, createQuestion } =
+    useQuestions(params.pollId);
 
   return (
     <div className="poll-container">
-      <h1>{pollQuery.data?.name}</h1>
+      <h1>{pollData?.name}</h1>
       <div className="question-container">
-        {questionsQuery.data?.map((q) => {
+        {questions?.map((q) => {
           return (
             <li key={q.id}>
               <div>{q.id}</div>
@@ -137,7 +25,7 @@ function Poll() {
               <Checkbox
                 checked={selectedQuestions.has(q.id)}
                 onChange={() => {
-                  toggleVoteMutation.mutate(q);
+                  toggleVote(q);
                 }}
               ></Checkbox>
             </li>
@@ -152,7 +40,9 @@ function Poll() {
             setInputText(e.target.value);
           }}
         />
-        <button onClick={() => createQuestionMutation.mutate()}>ADD</button>
+        <button onClick={() => createQuestion({ value: inputText })}>
+          ADD
+        </button>
       </div>
     </div>
   );
---
+++ client/src/poll/usePollData.tsx
@@ -0,0 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
+
+interface PollData {
+  id: string;
+  name: string;
+}
+function usePollData(pollId?: string) {
+  const getPollData = async (): Promise<PollData> => {
+    const response = await fetch(`/api/polls/${pollId}`, {
+      method: "GET",
+    });
+    const data = await response.json();
+    return data;
+  };
+  const pollQuery = useQuery({ queryKey: ["poll"], queryFn: getPollData });
+  return pollQuery.data;
+}
+export default usePollData;
